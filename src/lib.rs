58fe0ab4ada352830d0640d2b029fbd2bfc16459
Nathan Fuchs <fuchsnj@gmail.com>
2015-12-13 12:21:52 -0500

    local pubsub added

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate aws_dynamodb;
+extern crate pubsub;
 
 use aws_dynamodb::{DynamoDb, Table};
 
@@ -6,13 +7,53 @@ use aws_dynamodb::{DynamoDb, Table};
 fn it_works() {
 }
 
+struct Subscription{
+	sub: pubsub::Subscription
+}
+impl Subscription{
+	pub fn cancel(self){/* sub is dropped */}
+	
+	pub fn notify_others(&self, msg: &str){
+		self.sub.notify_others(msg)
+	}
+}
+
+struct SubActivator{
+	activator: pubsub::SubActivator
+}
+impl SubActivator{
+	pub fn activate<F>(self, func: F) -> Subscription
+	where F: FnMut(String) + 'static + Send{
+		Subscription{
+			sub: self.activate(func)
+		}
+	}
+}
+
+
 struct PubSub{
-	table: Table
+	table: Table,
+	pubsub: pubsub::PubSub
 }
 impl PubSub{
-	fn new(table: Table) -> PubSub{
+	pub fn new(table: Table, domain: String, num_threads: usize) -> PubSub{
 		PubSub{
-			table: table
+			table: table,
+			pubsub: pubsub::PubSub::new(num_threads)
+		}
+	}
+	pub fn subscribe<F>(&self, channel: &str, func: F) -> Subscription
+	where F: FnMut(String) + 'static + Send{
+		Subscription{
+			sub: self.pubsub.subscribe(channel, func)
+		}
+	}
+	pub fn notify(&self, channel: &str, msg: &str){
+		self.pubsub.notify(channel, msg)
+	}
+	pub fn lazy_subscribe(&self, channel: &str) -> SubActivator{
+		SubActivator{
+			activator: self.pubsub.lazy_subscribe(channel)
 		}
 	}
 }
